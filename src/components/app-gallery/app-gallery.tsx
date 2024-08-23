98174819816d6a090fb30fee58921305d3498903
Schouwey <zaid.schouwey@gmail.com>
2024-08-23 18:22:08 +0200

    general changes

--- src/components/app-gallery/app-gallery.tsx
+++ src/components/app-gallery/app-gallery.tsx
@@ -38,24 +38,34 @@ export class AppGallery {
     if (this.albums.length === 0)
       return <div class="container-fluid">Loading...</div>;
     for (let i = 0; i < this.albums.length; i++) {
-      if(!this.albums[i].hasOwnProperty("photos")){
+      if (!this.albums[i].hasOwnProperty("photos")) {
         this.albums.splice(i, 1);
       }
     }
     return (
       <div class="container-fluid" >
+        <div class="row bg-1-titles">
+          <h1>Galerie de Nos Chats Bengals : Photos de Chatons et Adultes</h1>
+
+          <p>Bienvenue dans la galerie de notre élevage de chats Bengals ! Découvrez ici une sélection de photos mettant en avant nos magnifiques chatons et chats adultes. Chaque image capture la beauté, le caractère et le charme unique de nos Bengals, élevés avec passion et dévouement.</p>
+
+          <p>Parcourez nos albums pour admirer les différentes robes et personnalités de nos compagnons félins. Nos chatons sont photographiés à différents stades de leur développement, montrant leur évolution et leurs traits distinctifs. Vous trouverez également des portraits de nos chats adultes, illustrant leur élégance et leur caractère exceptionnel.</p>
+
+          <p>Nous espérons que ces images vous donneront un aperçu de la qualité et de la beauté de nos chats Bengals. Si vous avez des questions sur nos chats ou si vous souhaitez en savoir plus sur l’un d’entre eux, n’hésitez pas à nous contacter.</p>
+
+          <p>Merci de votre visite et profitez de la découverte de nos précieux félins !</p></div>
         {
           this.albums.map((album, i) => {
-              return (
-                <div class={`row ${(i + 1) % 2 === 0 ? "bg-2" : "bg-1"} text-center`}>
-                  <h3>Album {album.name}</h3>
-                  <div class="text-center" id={`static-thumbnails-${i}`}>
-                    {album.photos?.data?.map((photo: any) => {
-                      return <a href={photo.source}><img src={photo.source}></img></a>
-                    })}
-                  </div>
+            return (
+              <div class={`row ${(i + 1) % 2 === 0 ? "bg-1" : "bg-2"} text-center`}>
+                <h2>Album {album.name}</h2>
+                <div class="text-center" id={`static-thumbnails-${i}`}>
+                  {album.photos?.data?.map((photo: any) => {
+                    return <a href={photo.source}><img src={photo.source}></img></a>
+                  })}
                 </div>
-              )
+              </div>
+            )
           }
           )
         }
--- src/components/app-kittens/app-kittens.tsx
+++ src/components/app-kittens/app-kittens.tsx
@@ -28,7 +28,7 @@ export class AppKittens {
   async componentWillLoad() {
     if (sessionStorage.getItem('albums')) {
       const kittensAlb = JSON.parse(sessionStorage.getItem('albums')).find((album: any) => album.name === 'Chatons à placer et chats pour la retraite');
-      if(kittensAlb === undefined){
+      if (kittensAlb === undefined) {
         console.error("Album chatons not found.");
       }
       this.kittens = this.albumToKittens(kittensAlb);
@@ -52,16 +52,25 @@ export class AppKittens {
     )
   }
   render() {
-    console.log(this.kittens)
-    //
-    if(!this.kittens)
+    if (!this.kittens)
       return (<div>Loading...</div>)
     return (
       <div class="container-fluid">
-        {this.kittens.map((kitten,i) =>{
+        <div class="row bg-1-titles">
+          <h1>Chatons Bengals Disponibles : Découvrez Nos Petits Compagnons à Adopter</h1>
+
+          <p>Bienvenue sur notre page dédiée aux chatons Bengals actuellement disponibles à l'adoption ! Nous sommes ravis de vous présenter nos adorables petits compagnons qui cherchent leur nouvelle famille. Chaque chaton est élevé avec soin et affection, prêt à rejoindre un foyer aimant.</p>
+
+          <p>Explorez les photos ci-dessous pour découvrir nos chatons actuels. Chaque image met en valeur les traits uniques de nos bébés Bengals, de leurs motifs élégants à leur personnalité charmante. Nous nous engageons à vous fournir des informations détaillées sur chaque chaton, y compris ses caractéristiques, son pedigree, et son tempérament.</p>
+
+          <p>Nos chatons sont élevés dans un environnement familial, garantissant qu'ils sont bien socialisés et prêts à s’adapter à leur nouvelle vie avec vous. Si l'un de nos chatons vous intéresse ou si vous souhaitez obtenir plus de renseignements, n'hésitez pas à nous contacter.</p>
+
+          <p>Nous sommes impatients de vous aider à trouver le compagnon parfait parmi nos chatons Bengals disponibles. Merci de votre intérêt et bonne visite !</p>
+        </div>
+        {this.kittens.map((kitten, i) => {
           return (
-            <div class={`row ${(i + 1) % 2 === 0 ? "bg-2" : "bg-1"} text-center`}>
-              <h3>Chatons disponibles</h3>
+            <div class={`row ${(i + 1) % 2 === 0 ? "bg-1" : "bg-2"} text-center`}>
+              <h2>Chatons disponibles</h2>
               <div class="text-center" id={`static-thumbnails-${i}`}>
                 {kitten.photos.map((photo: any) => {
                   return <a href={photo.source}><img src={photo.source}></img></a>
@@ -70,7 +79,7 @@ export class AppKittens {
             </div>
           )
         })}
-        
+
       </div>
     );
   }
